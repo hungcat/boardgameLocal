2ca3e8f5195daa4148636bc467d149825ecf6886
hungcat <hungcat4sheep@gmail.com>
2017-08-18 18:45:58 +0900

    add default common/config.ts

---
+++ src/common/config.ts
@@ -0,0 +1,10 @@
+export module ClientConfig {
+  export const wsHost: string = '';
+};
+
+export module ServerConfig {
+  export const serverPort: string = '60011';
+  export const isHTTPS: boolean = false;
+  export const httpsKey: string = '';
+  export const httpsCert: string = '';
+};
