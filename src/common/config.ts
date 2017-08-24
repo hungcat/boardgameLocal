551e4c64a652f4109ce4fcdaf405b276c466b3ae
hungCat <hungcat4sheep@gmail.com>
2017-08-24 22:35:37 +0900

    Update config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,9 +1,11 @@
+const port: string = '3000';
+
 export module ClientConfig {
-  export const wsHost: string = '';
+  export const wsHost: string = 'ws://localhost:' + port + '/ws';
 };
 
 export module ServerConfig {
-  export const serverPort: string = '60011';
+  export const serverPort: string = port;
   export const isHTTPS: boolean = false;
   export const httpsKey: string = '';
   export const httpsCert: string = '';
